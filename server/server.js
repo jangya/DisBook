f1cc1dfb083cb7adac9342372c5a6f0038f8d4ad
i354110 <jangya.narayana.satapathy@sap.com>
2021-01-24 14:51:31 +0530

    react client config

--- server/server.js
+++ server/server.js
@@ -64,6 +64,14 @@ app.put('/api/books/:id', (req, res) => {
     });
   }, true);
 });
-
+if (process.env.NODE_ENV === 'production') {
+  // Serve any static files
+  app.use(express.static(path.join(__dirname, 'client/build')));
+    
+  // Handle React routing, return all requests to React app
+  app.get('*', function(req, res) {
+    res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
+  });
+}
 
 app.listen(port, () => console.log(`Listening on port ${port}`));
